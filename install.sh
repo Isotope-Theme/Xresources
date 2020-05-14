bd286d8ecf621f6f4c3770d2ee9c96f424c44584
Arden Rasmussen <ardenisthebest@gmail.com>
2020-05-14 14:05:17 -0700

    Added light variant

--- install.sh
+++ install.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-echo "Installing Isotope theme to $HOME/.Xresources"
-curl -s https://raw.githubusercontent.com/Isotope-Theme/xresources/master/Xresources >> $HOME/.Xresources
+if [[ ! -d "$HOME/" ]]; then
+  mkdir -p "$HOME/"
+fi
+VARIANTS=("dark" "light" "quit")
+select variant in "${VARIANTS[@]}"; do
+  case $variant in
+    "dark")
+      echo "Installing Isotope Dark theme to $HOME/.Xresources"
+      curl -s https://raw.githubusercontent.com/Isotope-Theme/xresources/master/Xresources-dark >> $HOME/.Xresources
+      break
+      ;;
+    "light")
+      echo "Installing Isotope Light theme to $HOME/.Xresources"
+      curl -s https://raw.githubusercontent.com/Isotope-Theme/xresources/master/Xresources-light.conf >> $HOME/.Xresources
+      break
+      ;;
+    "quit")
+      break
+      ;;
+    *)
+      echo "invalid variant $REPLY"
+      ;;
+  esac
+done
